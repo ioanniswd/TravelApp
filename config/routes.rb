6cf4cc0234e26df7d4f64df61a73e36a894fcca0
ioanniswd <ioanniswd@gmail.com>
2019-01-15 00:56:23 +0200

    Added destination show and link to destination in show of tag.
     On branch master
    
     Changes to be committed:
    	new file:   app/assets/javascripts/destinations.coffee
    	new file:   app/assets/stylesheets/destinations.scss
    	new file:   app/controllers/destinations_controller.rb
    	new file:   app/helpers/destinations_helper.rb
    	new file:   app/views/destinations/show.html.erb
    	modified:   app/views/tags/show.html.erb
    	modified:   config/routes.rb
    	new file:   test/controllers/destinations_controller_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/tags' => 'tags#index'
   get '/tags/:id' => 'tags#show', as: :tag
+  get '/destinations/:id' => 'destinations#show', as: :destination
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
