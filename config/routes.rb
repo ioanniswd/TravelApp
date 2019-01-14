38a1e5fbb2643404e3a3d0eec95af8b2b80ecd53
ioanniswd <ioanniswd@gmail.com>
2019-01-15 00:29:25 +0200

    Added tags index and show.
     On branch master
    
     Changes to be committed:
    	new file:   app/assets/javascripts/tags.coffee
    	modified:   app/assets/stylesheets/application.css
    	new file:   app/assets/stylesheets/tags.scss
    	new file:   app/controllers/tags_controller.rb
    	new file:   app/helpers/tags_helper.rb
    	new file:   app/views/tags/index.html.erb
    	new file:   app/views/tags/show.html.erb
    	modified:   config/routes.rb
    	new file:   test/controllers/tags_controller_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/tags' => 'tags#index'
+  get '/tags/:id' => 'tags#show', as: :tag
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
